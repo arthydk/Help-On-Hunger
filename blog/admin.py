a6d57a154b069abe61f669da2b8b24d3fdc3a157
arthydk <115569766+arthydk@users.noreply.github.com>
2025-01-23 23:07:58 +0530

    Delete blog directory

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from blog.models import dform,contact,customer
-
-
-admin.site.register(dform)
-admin.site.register(contact)
-admin.site.register(customer)
